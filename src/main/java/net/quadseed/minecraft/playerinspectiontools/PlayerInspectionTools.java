575a18d9594dac9e3c46f28b90c18795feef2c40
quadseed <46034259+quadseed@users.noreply.github.com>
2022-02-22 17:58:51 +0900

    Initial Commit

---
+++ src/main/java/net/quadseed/minecraft/playerinspectiontools/PlayerInspectionTools.java
@@ -0,0 +1,14 @@
+package net.quadseed.minecraft.playerinspectiontools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerInspectionTools extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}

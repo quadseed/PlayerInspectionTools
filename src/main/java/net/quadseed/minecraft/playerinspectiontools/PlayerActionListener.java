01d8350ef847a09ff19a29653ca263a48cb6cfaf
quadseed <46034259+quadseed@users.noreply.github.com>
2022-02-23 18:32:03 +0900

    Add Inventory Action Handler

---
+++ src/main/java/net/quadseed/minecraft/playerinspectiontools/PlayerActionListener.java
@@ -0,0 +1,29 @@
+package net.quadseed.minecraft.playerinspectiontools;
+
+import net.quadseed.minecraft.playerinspectiontools.menugui.BaseMenu;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.InventoryHolder;
+
+public class PlayerActionListener implements Listener {
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        InventoryHolder holder = event.getInventory().getHolder();
+        if (holder instanceof BaseMenu) {
+            BaseMenu menu = (BaseMenu) holder;
+            menu.InventoryClickHandler(event);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        InventoryHolder holder = event.getInventory().getHolder();
+        if (holder instanceof BaseMenu) {
+            BaseMenu menu = (BaseMenu) holder;
+            menu.InventoryCloseHandler(event);
+        }
+    }
+}
